d96926a8c6f37317cd150a98d2942400c99fb4d9
Alex <alexgrado39@gmail.com>
2023-11-20 17:05:46 +0100

    Added websocket communication with front

--- src/main/java/com/chess/presentation/BoardController.java
+++ src/main/java/com/chess/presentation/BoardController.java
@@ -1,5 +1,15 @@
 package com.chess.presentation;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class BoardController {
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public String greeting(String message) throws Exception {
+        return "Acabas de enviarme el mensaje: " + message;
+    }
 }
